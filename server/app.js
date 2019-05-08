1a240fd984917857386d22b75a8eaed07199723a
MiracleAyodele <miracleaayodele@gmail.com>
2019-05-08 13:04:08 +0000

    app.js working as server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 /* eslint no-restricted-globals: 0 */
 /* eslint indent: 0 */
 /* eslint no-tabs: 0 */
-/* eslint no-tabs: 0 */
+/* eslint no-unused-vars: 0 */
 /* eslint no-mixed-spaces-and-tabs: 0 */
+/* eslint arrow-body-style:0 */
 const express = require('express');
 const mongoose = require('mongoose');
 const expressValidator = require('express-validator');
@@ -13,58 +14,64 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
+const next = require('next');
 const devConfig = require('../webpack.config.babel');
+const routes = require('./routes');
+const configDB = require('./config/database'); // database config
 
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = parseInt(process.env.PORT, 10) || 3000;
+const server = express(); // Set up the express app
 dotenv.config();
-
-const port = parseInt(process.env.PORT, 10) || 8000;
-
-// database config
-const configDB = require('./config/database');
-
-// Set up the express app
-const app = express();
 
 mongoose.Promise = global.Promise;
 let compiler;
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 if (process.env.NODE_ENV === 'production') {
-	mongoose.connect(configDB.url_production);
+	mongoose.connect(configDB.url_production, { useNewUrlParser: true });
 } else if (process.env.NODE_ENV === 'test') {
-	mongoose.connect(configDB.url_test);
+	mongoose.connect(configDB.url_test, { useNewUrlParser: true });
   	compiler = webpack(devConfig);
 } else {
-  	mongoose.connect(configDB.url);
+  	mongoose.connect(configDB.url, { useNewUrlParser: true });
   	compiler = webpack(devConfig);
 }
+
 // Log requests to the console.
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressValidator());
-app.use(express.static(__dirname + './../public'));
-app.use('/api-docs', express.static(path.join(__dirname, './../api-docs')));
+server.use(logger('dev'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(expressValidator());
+server.use(express.static(path.join(__dirname, './../public')));
+server.use('/api-docs', express.static(path.join(__dirname, './../api-docs')));
 
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-  app.use(webpackDevMiddleware(compiler, {
-	  open: false,
-	  // publicPath: devConfig.output.publicPath,
-  }));
-  app.use(webpackHotMiddleware(compiler));
+	server.use(webpackDevMiddleware(compiler, {
+		open: false,
+	  	publicPath: devConfig.output.publicPath,
+  	}));
+  	server.use(webpackHotMiddleware(compiler));
 }
-app.use('/api/v1', routes);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
-});
 
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err, 'but stuff works'); //eslint-disable-line
-  } else {
-    console.log(`We are live on port ${port}...`.green); //eslint-disable-line
-  }
+// module.exports = app;
+
+app.prepare().then(() => {
+    server.get('*', (req, res) => {
+    	return handle(req, res);
+    	// Not sure this is needed anymore, since next does the job
+    	// res.sendFile(path.join(__dirname, '../client/index.html'));
+    });
+
+    server.listen(port, err => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${port}`);
+    });
+}).catch(ex => {
+    console.error(ex); /* eslint no-console:0 */
+    process.exit(1);
 });
 
 module.exports = app;
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,57 +1,69 @@
-import webpack from 'webpack';
-import path from 'path';
+/* eslint no-restricted-globals: 0 */
+/* eslint indent: 0 */
+/* eslint no-tabs: 0 */
+/* eslint max-len: 0 */
+/* eslint no-mixed-spaces-and-tabs: 0 */
+const webpack = require('webpack');
+const path = require('path');
 
 const debug = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  devtool: debug ? 'inline-sourcemap' : false,
-  entry: [
-    'eventsource-polyfill', // necessary for hot reloading with IE
-    'webpack-hot-middleware/client?reload=true',
-    path.resolve(__dirname, './client/js/app/index.js')
-  ],
+	devServer: {
+		contentBase: path.resolve(__dirname, 'client'),
+		historyApiFallback: true,
+	},
+
+	devtool: debug ? 'inline-sourcemap' : false,
+
+	entry: [
+		'eventsource-polyfill', // necessary for hot reloading with IE
+		'webpack-hot-middleware/client?reload=true',
+		path.resolve(__dirname, './client/js/app/index.js'),
+	],
 
   module: {
     loaders: [
-      { test: /\.(css|scss)$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] },
       {
-        test: /\.(js|jsx)$/, include: path.join(__dirname, 'client'), loader: 'babel-loader', query: { presets: ['es2015', 'react'] }
+		  loaders: ['style-loader', 'css-loader', 'sass-loader'],
+		  test: /\.(css|scss)$/,
+	  },
+      {
+		  include: path.join(__dirname, 'client'),
+		  loader: 'babel-loader',
+		  query: { presets: ['es2015', 'react'] },
+		  test: /\.(js|jsx)$/,
+	  },
+      {
+		  loaders: 'file-loader?name=fonts/[name].[ext]',
+		  test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
       },
       {
-        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loaders: 'file-loader?name=fonts/[name].[ext]'
+		  loaders: [
+			  'file-loader',
+			  'image-webpack-loader',
+		  ],
+		  test: /\.(gif|png|jpg|svg)$/i,
       },
-      {
-        test: /\.(gif|png|jpg|svg)$/i,
-        loaders: [
-          'file-loader',
-          'image-webpack-loader',
-        ],
-      }
-    ]
+    ],
   },
 
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, 'client')
-  },
+	node: {
+		dns: 'empty',
+		net: 'empty',
+	},
 
   output: {
-    path: `${__dirname}/client/dist/`,
-    filename: 'bundle.js',
-    publicPath: '/'
+	  filename: 'bundle.js',
+	  path: `${__dirname}/client/dist/`,
+	  publicPath: '/',
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+  	new webpack.HotModuleReplacementPlugin(),
   ],
 
   resolve: {
-    extensions: ['.js', '.json', '.jsx']
+    extensions: ['.js', '.json', '.jsx'],
   },
-
-  node: {
-    net: 'empty',
-    dns: 'empty'
-  }
 };
