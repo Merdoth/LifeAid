851120ab6acc548030228b254cf98956cf145d38
Ucheya <chimereucheya.okereke@shuttlers.ng>
2019-06-01 16:27:41 +0100

    Remove unnecessary code files and lines, add new code files and images

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,124 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable max-len */
 /* eslint react/jsx-filename-extension: 0 */
 /* eslint react/jsx-no-literals: 0 */
 /* eslint arrow-body-style: 0 */
+// eslint-disable-next-line react/jsx-no-comment-textnodes
+// eslint-disable-next-line react/jsx-no-undef
+// eslint-disable-next-line jsx-a11y/anchor-is-valid
+// eslint-disable-next-line react/button-has-type
+
 import React from 'react';
-import Auth from '../server/authentication/Auth';
+import '../scss/home.scss';
 
-const auth = new Auth();
-
-class Callback extends React.Component {
-    componentDidMount() {
-        auth.handleAuthentication();
-    }
-
-    render() {
-        return (
-            <div>Life Aid</div>
-        );
-    }
-}
-
-const Index = () => {
+const Home = () => {
     return (
-        <div>
-            <Callback />
-            <div>
-                <button type="button" onClick={() => auth.login()}>Login</button>
+        <React.Fragment>
+            <div className="wrapper">
+                <div className="header">
+                    <div className="header-cover">
+                        <p className="header-cover-text">
+                        Emergency First Aid
+                            <br />
+                            {' '}
+                        solutions at your
+                            <br />
+                        doorsteps.
+                        </p>
+                        <button>READ MORE</button>
+                    </div>
+                </div>
+                <div className="articles app-content" id="articles">
+                    <hr />
+                    <p>Our Featured Articles</p>
+                    <div className="content-wrapper">
+                        <div className="article-content article1">
+                            <p className="article-content-header">Why First Aid?</p>
+                            <p className="article-read">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc non blandit massa enim. Donec enim diam vulputate ut pharetra sit. Id faucibus nisl tincidunt eget nullam. Vitae congue eu consequat ac felis donec et odio pellentesque. Cursus vitae congue mauris rhoncus aenean vel elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc non blandit massa enim. Donec enim diam vulputate ut pharetra sit. Id faucibus nisl tincidunt eget nullam. Vitae congue eu consequat ac felis donec et odio pellentesque. Cursus vitae.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc non blandit massa enim. Donec enim diam vulputate ut pharetra sit. Id faucibus nisl tincidunt eget nullam. Vitae congue eu consequat ac felis donec et odio pellentesque. Cursus vitae congue mauris
+                                <a className="article-content-a"> Read more...</a>
+                            </p>
+                        </div>
+                        <div className="article-content">
+                            <p className="article-content-header">What Should be in a First Aid kit?</p>
+                            <p className="article-read">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc non blandit massa enim. Donec enim diam vulputate ut pharetra sit. Id faucibus nisl tincidunt eget nullam. Vitae congue eu consequat ac felis donec et odio pellentesque. Cursus vitae congue mauris rhoncus aenean vel elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc non blandit massa enim. Donec enim diam vulputate ut pharetra sit. Id faucibus nisl tincidunt eget nullam. Vitae congue eu consequat ac felis donec et odio pellentesque. Cursus vitae.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc non blandit massa enim. Donec enim diam vulputate ut pharetra sit. Id faucibus nisl tincidunt eget nullam. Vitae congue eu consequat ac felis donec et odio pellentesque. Cursus vitae congue mauris
+                                <a className="article-content-a"> Read more...</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="services app-content">
+                    <hr />
+                    <p>Our Featured Services</p>
+                    <div className="content-wrapper services-wrapper">
+                        <div className="services-content">
+                            <img className="service-img" src="/static/img/help.jpg" alt="Smiley face" />
+                            <span className="services-content-text">
+                                <p className="service-text1">
+Emergency Contact
+                                    <br />
+                                    {' '}
+Number
+                                </p>
+                                <p className="service-text2">Help Desk</p>
+                            </span>
+                        </div>
+                        <div className="services-content">
+                            <img className="service-img" src="/static/img/cardiac.jpg" alt="Smiley face" />
+                            <span className="services-content-text">
+                                <p className="service-text1">
+Instructions On How To
+                                    <br />
+                                    {' '}
+Administer First Aid
+                                </p>
+                                <p className="service-text2">Help Desk</p>
+                            </span>
+                        </div>
+                        <div className="services-content">
+                            <img className="service-img" src="/static/img/care.jpg" alt="Smiley face" />
+                            <span className="services-content-text">
+                                <p className="service-text1">
+Opportunity To Ask Doctors
+                                    <br />
+                                    {' '}
+Questions
+                                </p>
+                                <p className="service-text2">Help Desk</p>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="intro app-content">
+                    <hr />
+                    <p className="intro-header">
+A great knowledge of what to do during emergencies
+                        <br />
+                        {' '}
+can save you alot of deaths there’s why;
+                    </p>
+                    <span>
+                        <p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc non blandit massa enim. Donec enim diam vulputate ut pharetra sit. Id faucibus nisl tincidunt eget nullam. Vitae congue eu consequat ac felis donec et odio pellentesque. Cursus vitae congue mauris rhoncus aenean vel elit. Dui faucibus in ornare quam viverra. In est ante in nibh mauris cursus. Vel pharetra vel turpis nunc eget lorem. Purus ut faucibus pulvinar elementum.Nunc non blandit massa enim. Donec enim diam vulputate ut pharetra sit. Id faucibus nisl tincidunt eget nullam. Vitae congue eu consequat ac felis donec et odio pellentesque. Cursus vitae congue mauris rhoncus aenean vel elit Nibh ipsum consequat nisl vel pretium.Vitae congue eu consequat ac felis donec et odio pellentesque. Cursus vitae congue mauris rhoncus aenean vel elit. Dignissim sodales ut eu sem integer,
+                            {' '}
+                            <a className="article-content-a">  Read more...</a>
+                        </p>
+                    </span>
+                </div>
+                <div className="consult app-content">
+                    <div className="consult-text">
+                        <p className="consult-text1">Need A Personal Health Plan? or A Counselling Session?</p>
+                        <p className="consult-text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <button>READ MORE</button>
+                </div>
             </div>
-        </div>
+        </React.Fragment>
     );
 };
 
-export default Index;
+export default Home;
