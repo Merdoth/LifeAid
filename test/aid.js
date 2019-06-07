4c2ccae873c7dee5f358efc5678a5fc0bdbc9d54
codemutatio <faithomojola@gmail.com>
2019-06-07 15:24:45 +0100

    aid routes test

--- test/aid.js
+++ test/aid.js
@@ -1,4 +1,3 @@
-/* eslint-disable sort-keys */
 import assert from 'assert';
 import fetch from 'node-fetch';
 import nock from 'nock';
@@ -17,22 +16,22 @@ describe('aids', () => {
             */
             const testAidList = [
                 {
-                    title: 'Suffocation',
-                    intro: 'Asphyxia is a condition where the oxygen level',
-                    video: 'https://youtu.be/Dfod4noPe8E',
+                    _id: 1,
                     description: 'Lorem ipsum dolor sit amet,',
                     imageId: 'audio/i8nuguxuuie6lqjzzj19',
-                    imageUr: 'http://res.cloudinary.com',
-                    __v: 0,
+                    imageUrl: 'http://res.cloudinary.com',
+                    intro: 'Asphyxia is a condition where the oxygen level',
+                    title: 'Suffocation',
+                    video: 'https://youtu.be/Dfod4noPe8E',
                 },
                 {
-                    title: 'Suffocation',
-                    intro: 'Asphyxia is a condition where the oxygen level',
-                    video: 'https://youtu.be/Dfod4noPe8E',
+                    _id: 2,
                     description: 'Lorem ipsum dolor sit amet,',
                     imageId: 'audio/i8nuguxuuie6lqjzzj19',
-                    imageUr: 'http://res.cloudinary.com',
-                    __v: 0,
+                    imageUrl: 'http://res.cloudinary.com',
+                    intro: 'Asphyxia is a condition where the oxygen level',
+                    title: 'Suffocation',
+                    video: 'https://youtu.be/Dfod4noPe8E',
                 },
             ];
 
@@ -48,28 +47,72 @@ describe('aids', () => {
     });
     describe('GET /aids/:id', () => {
         it('should GET an aid by the given id', async () => {
-            const testAidList = {
-                title: 'Suffocation',
-                intro: 'Asphyxia is a condition where the oxygen level',
-                video: 'https://youtu.be/Dfod4noPe8E',
+            const testAid = {
+                _id: 1,
                 description: 'Lorem ipsum dolor sit amet,',
                 imageId: 'audio/i8nuguxuuie6lqjzzj19',
-                imageUr: 'http://res.cloudinary.com',
-                __v: 0,
+                imageUrl: 'http://res.cloudinary.com',
+                intro: 'Asphyxia is a condition where the oxygen level',
+                title: 'Suffocation',
+                video: 'https://youtu.be/Dfod4noPe8E',
             };
             scope
-                .get('/api/v1/aids/5ce0496cb8cc3f3d64408a74')
-                .reply(200, testAidList);
+                .get('/api/v1/aids/1')
+                .reply(200, testAid);
 
-            const response = await fetch(`${TEST_URL}/api/v1/aids/5ce0496cb8cc3f3d64408a74`);
+            const response = await fetch(`${TEST_URL}/api/v1/aids/1`);
             const data = await response.json();
 
-            assert.deepStrictEqual(data, testAidList);
+            assert.deepStrictEqual(data, testAid);
         });
     });
     describe('/PUT/:id aid', () => {
-        it('it should UPDATE a aid given the id', () => {
-            
+        it('it should UPDATE an aid given the id', async () => {
+            const updataeData = {
+                intro: 'intro udpdate',
+                title: 'Title Update',
+            };
+            const testAid = {
+                _id: 1,
+                description: 'Lorem ipsum dolor sit amet,',
+                imageId: 'audio/i8nuguxuuie6lqjzzj19',
+                imageUrl: 'http://res.cloudinary.com',
+                intro: 'Asphyxia is a condition where the oxygen level',
+                title: 'Suffocation',
+                video: 'https://youtu.be/Dfod4noPe8E',
+            };
+            scope
+                .put('/api/v1/aids/1')
+                .reply(200, { ...testAid, ...updataeData });
+
+            const response = await fetch(`${TEST_URL}/api/v1/aids/1`, {
+                body: updataeData,
+                method: 'PUT',
+            });
+            const data = await response.json();
+            assert.deepStrictEqual(data, { ...testAid, ...updataeData });
+        });
+    });
+    describe('/POST aid', () => {
+        it('it should CREATE an aid', async () => {
+            const testAid = {
+                description: 'Lorem ipsum dolor sit amet,',
+                imageId: 'audio/i8nuguxuuie6lqjzzj19',
+                imageUrl: 'http://res.cloudinary.com',
+                intro: 'Asphyxia is a condition where the oxygen level',
+                title: 'Suffocation',
+                video: 'https://youtu.be/Dfod4noPe8E',
+            };
+            scope
+                .post('/api/v1/aids')
+                .reply(200, testAid);
+
+            const response = await fetch(`${TEST_URL}/api/v1/aids`, {
+                body: testAid,
+                method: 'POST',
+            });
+            const data = await response.json();
+            assert.deepStrictEqual(data, testAid);
         });
     });
 });
