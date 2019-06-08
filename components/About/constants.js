4f40f6efaa6675d98b0687355cec544735c90f5f
Ucheya <chimereucheya.okereke@shuttlers.ng>
2019-06-08 14:43:13 +0100

    add about page file and css design Extract about page strings into reusable constants Fix some eslint restriction errors Fix css code syntax and indentation error Add global.css file to take general application design code

---
+++ components/About/constants.js
@@ -0,0 +1,18 @@
+/* eslint-disable max-len */
+/* eslint-disable import/prefer-default-export */
+export const ABOUT_STRING = {
+    CLIENT:
+    'Client ~',
+    INTRO_HEADER_TEXT:
+   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc non blandit massa enim. Donec enim diam vulputate ut pharetra sit. Id faucibus nisl tincidunt eget nullam. Vitae congue eu consequat ac felis donec et odio pellentesque. Cursus vitae congue mauris rhoncus aenean vel elit. Dui faucibus in ornare quam viverra. In est ante in nibh mauris cursus. Vel pharetra vel turpis nunc eget lorem. Purus ut faucibus pulvinar elementum.Nunc non blandit massa enim. Donec enim diam vulputate ut pharetra sit. Id faucibus nisl tincidunt eget nullam. Vitae congue eu consequat ac felis donec et odio pellentesque. Cursus vitae congue mauris rhoncus aenean vel elit Nibh ipsum consequat nisl vel pretium.Vitae congue eu consequat ac felis donec et odio pellentesque. Cursus vitae congue mauris rhoncus aenean vel elit. Dignissim sodales ut eu sem integer.',
+    INTRO_TEXT1:
+   'A great knowledge of what to do during emergencies',
+    INTRO_TEXT2:
+   'can save you alot of deaths there’s why;',
+    NINTY:
+   '95%',
+    TEST1:
+   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ante leo, finibus quis est ut, tempor tincidunt ipsum. Nam consequat semper sollicitudin. Aliquam nec dap ibus massa. Pellen tesque in luctus ex. Praesent luctus erat sit amet torto.Nam consequat semper sollicitudin. Aliquam nec dap ibus massa. Pellen tesque in luctus ex. Praesent luctus erat sit amet tortor aliquam bibendum.',
+    TEST2:
+   'Slor sit amet, consectetur adipiscing elit. Curabitur ante leo, finibus quis est ut, tempor tincidunt ipsum. Nam consequat semper sollicitudin. Aliquam nec dap ibus massa. Pellen tesque in luctus ex. Praesent luctus erat sit amet tortor aliquam bibendum.Aliquam nec dap ibus massa. Pellen tesque in luctus ex. Praesent luctus erat sit amet torto.',
+};
