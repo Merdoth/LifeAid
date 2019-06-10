7bb6351f2dca7c3eafb9b69c308300e2fc59fc17
codemutatio <faithomojola@gmail.com>
2019-06-10 12:24:08 +0100

    sms update

--- server/middlewares/constant.js
+++ server/middlewares/constant.js
@@ -1,7 +1,7 @@
 const sender = 'lifeaidd@gmail.com';
 const title = 'Emergency report';
 const receiver = 'faithomojola@yahoo.com';
-const smsBody = 'Your report has been recieved and the relevant response agencies have been contacted, Help is on it way';
+const smsBody = 'Your report has been recieved and Help is on it way';
 
 module.exports = {
     receiver,
