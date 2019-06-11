7664c1e96f7ee301ac85147d31ececc3c63a25ce
codemutatio <faithomojola@gmail.com>
2019-06-11 12:44:08 +0100

    mail array

--- server/middlewares/constant.js
+++ server/middlewares/constant.js
@@ -1,9 +1,37 @@
 const sender = 'lifeaidd@gmail.com';
 const title = 'Emergency report';
-const receiver = 'faithomojola@yahoo.com';
+const smsBody = 'Your report has been recieved and Help is on it way';
+const receiver = [
+    {
+        address: 'faithomojola@yahoo.com',
+        name: 'Faith Omojola',
+
+    },
+    {
+        address: 'miracleaayodele@gmail.com',
+        name: 'Miracle Ayodele',
+
+    },
+    {
+        address: 'admin@enye.tech',
+        name: 'Enye Admin',
+
+    },
+    {
+        address: 'ucheya97@gmail.com',
+        name: 'Meya Samuel',
+
+    },
+    {
+        address: 'okemmadueric@gmail.com',
+        name: 'Okemmadu Eric',
+
+    },
+];
 
 module.exports = {
     receiver,
     sender,
+    smsBody,
     title,
 };
