5d9e65c3daf541380c9a3c3c2f155ca60ee06fd3
Ucheya <chimereucheya.okereke@shuttlers.ng>
2019-06-20 12:11:24 +0100

    Add report section to application Add new css design for the report section and made adjustments to the old designs Add antd design package to the application inorder to have access to the pagination component

--- components/Article/ArticlePage.js
+++ components/Article/ArticlePage.js
@@ -1,89 +1,118 @@
 import React from 'react';
+import Head from 'next/head';
+import 'antd/dist/antd.css';
+import { Pagination } from 'antd';
 import NavigationBar from '../Common/NavigationBar';
+import { REPORT } from './constants';
 import { HOME_STRINGS } from '../constants';
+import { SERVICE_STRING } from '../Service/constants';
+
 import './article.scss';
+
+const {
+    REPORT_TEXT,
+} = REPORT;
+
+const {
+    GOOGLE_FONTS,
+} = SERVICE_STRING;
 
 const {
     ARTICLE_READ,
     READ_MORE,
 } = HOME_STRINGS;
 
-const ArticlePage = () => {
-    return (
-        <div>
+const ArticlePage = () => (
+    <React.Fragment>
+        <Head>
+            <link href={GOOGLE_FONTS} />
+        </Head>
+        <div className="header">
             <NavigationBar />
-            <div className="articles app-content" id="articles">
-                <p>Our Featured Articles</p>
-                <div className="content-wrapper">
-                    <div className="article-content article1">
-                        <p className="article-content-header">Why First Aid?</p>
-                        <p className="article-read">
-                            {ARTICLE_READ}
-                            <a className="article-content-a" href="/articlePage">
-                                {READ_MORE}
-                                ...
-                            </a>
-                        </p>
-                    </div>
-                    <div className="article-content">
-                        <p className="article-content-header">What Should be in a First Aid kit?</p>
-                        <p className="article-read">
-                            {ARTICLE_READ}
-                            <a className="article-content-a" href="/articlePage">
-                                {READ_MORE}
-                                ...
-                            </a>
-                        </p>
-                    </div>
+            <div className="header-cover">
+
+                <p className="text1">
+                    {REPORT_TEXT}
+                </p>
+                <div className="input-container">
+                    <input type="file" id="real-input" />
+                    <button className="browse-btn" type="button">
+                      SEND FILE
+                    </button>
                 </div>
-                <div className="content-wrapper">
-                    <div className="article-content article1">
-                        <p className="article-content-header">Why First Aid?</p>
-                        <p className="article-read">
-                            {ARTICLE_READ}
-                            <a className="article-content-a" href="/articlePage">
-                                {READ_MORE}
-                                ...
-                            </a>
-                        </p>
-                    </div>
-                    <div className="article-content">
-                        <p className="article-content-header">What Should be in a First Aid kit?</p>
-                        <p className="article-read">
-                            {ARTICLE_READ}
-                            <a className="article-content-a" href="/articlePage">
-                                {READ_MORE}
-                                ...
-                            </a>
-                        </p>
-                    </div>
-                </div>
-                <div className="content-wrapper">
-                    <div className="article-content article1">
-                        <p className="article-content-header">Why First Aid?</p>
-                        <p className="article-read">
-                            {ARTICLE_READ}
-                            <a className="article-content-a" href="/articlePage">
-                                {READ_MORE}
-                                ...
-                            </a>
-                        </p>
-                    </div>
-                    <div className="article-content">
-                        <p className="article-content-header">What Should be in a First Aid kit?</p>
-                        <p className="article-read">
-                            {ARTICLE_READ}
-                            <a className="article-content-a" href="/articlePage">
-                                {READ_MORE}
-                                ...
-                            </a>
-                        </p>
-                    </div>
-                </div>
-                <p className="pagination"> 1  |  2  |  3  |  4  |  ...</p>
             </div>
         </div>
-    );
-};
+        <div className="articles app-content" id="articles">
+            <p>Our Featured Articles</p>
+            <div className="content-wrapper">
+                <div className="article-content article1">
+                    <p className="article-content-header">Why First Aid?</p>
+                    <p className="article-read">
+                        {ARTICLE_READ}
+                        <a className="article-content-a" href="/articlePage">
+                            {READ_MORE}
+                                ...
+                        </a>
+                    </p>
+                </div>
+                <div className="article-content">
+                    <p className="article-content-header">What Should be in a First Aid kit?</p>
+                    <p className="article-read">
+                        {ARTICLE_READ}
+                        <a className="article-content-a" href="/articlePage">
+                            {READ_MORE}
+                                ...
+                        </a>
+                    </p>
+                </div>
+            </div>
+            <div className="content-wrapper">
+                <div className="article-content article1">
+                    <p className="article-content-header">Why First Aid?</p>
+                    <p className="article-read">
+                        {ARTICLE_READ}
+                        <a className="article-content-a" href="/articlePage">
+                            {READ_MORE}
+                                ...
+                        </a>
+                    </p>
+                </div>
+                <div className="article-content">
+                    <p className="article-content-header">What Should be in a First Aid kit?</p>
+                    <p className="article-read">
+                        {ARTICLE_READ}
+                        <a className="article-content-a" href="/articlePage">
+                            {READ_MORE}
+                                ...
+                        </a>
+                    </p>
+                </div>
+            </div>
+            <div className="content-wrapper">
+                <div className="article-content article1">
+                    <p className="article-content-header">Why First Aid?</p>
+                    <p className="article-read">
+                        {ARTICLE_READ}
+                        <a className="article-content-a" href="/articlePage">
+                            {READ_MORE}
+                                ...
+                        </a>
+                    </p>
+                </div>
+                <div className="article-content">
+                    <p className="article-content-header">What Should be in a First Aid kit?</p>
+                    <p className="article-read">
+                        {ARTICLE_READ}
+                        <a className="article-content-a" href="/articlePage">
+                            {READ_MORE}
+                                ...
+                        </a>
+                    </p>
+                </div>
+            </div>
+            <Pagination className="pagination" defaultCurrent={1} total={3} />
+        </div>
+    </React.Fragment>
+);
 
 export default ArticlePage;
