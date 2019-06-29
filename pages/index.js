623478fe139bb221bd223b7db0c81a16a607857e
Ucheya <chimereucheya.okereke@shuttlers.ng>
2019-06-29 18:23:26 +0100

    fix application development branch errors and bugs

--- pages/index.js
+++ pages/index.js
@@ -64,46 +64,46 @@ const Home = () => (
                         </p>
                     </div>
                 </div>
-                <div className="services app-content">
-                    <hr />
-                    <p>{SERVICES}</p>
-                    <div className="content-wrapper services-wrapper">
-                        <div className="services-content">
-                            <img className="service-img" src="/static/img/help.jpg" alt="Smiley face" />
-                            <span className="services-content-text">
-                                <p className="service-text1">
+            </div>
+            <div className="services app-content">
+                <hr />
+                <p>{SERVICES}</p>
+                <div className="content-wrapper services-wrapper">
+                    <div className="services-content">
+                        <img className="service-img" src="/static/img/help.jpg" alt="Smiley face" />
+                        <span className="services-content-text">
+                            <p className="service-text1">
                                     Emergency Contact
-                                    <br />
-                                    {' '}
+                                <br />
+                                {' '}
                                     Number
-                                </p>
-                                <p className="service-text2">{HELP_DESK}</p>
-                            </span>
-                        </div>
-                        <div className="services-content">
-                            <img className="service-img" src="/static/img/cardiac.jpg" alt="Smiley face" />
-                            <span className="services-content-text">
-                                <p className="service-text1">
+                            </p>
+                            <p className="service-text2">{HELP_DESK}</p>
+                        </span>
+                    </div>
+                    <div className="services-content">
+                        <img className="service-img" src="/static/img/cardiac.jpg" alt="Smiley face" />
+                        <span className="services-content-text">
+                            <p className="service-text1">
                                     Instructions On How To
-                                    <br />
-                                    {' '}
+                                <br />
+                                {' '}
                                     Administer First Aid
-                                </p>
-                                <p className="service-text2">{HELP_DESK}</p>
-                            </span>
-                        </div>
-                        <div className="services-content">
-                            <img className="service-img" src="/static/img/care.jpg" alt="Smiley face" />
-                            <span className="services-content-text">
-                                <p className="service-text1">
+                            </p>
+                            <p className="service-text2">{HELP_DESK}</p>
+                        </span>
+                    </div>
+                    <div className="services-content">
+                        <img className="service-img" src="/static/img/care.jpg" alt="Smiley face" />
+                        <span className="services-content-text">
+                            <p className="service-text1">
                                     Opportunity To Ask Doctors
-                                    <br />
-                                    {' '}
+                                <br />
+                                {' '}
                                     Questions
-                                </p>
-                                <p className="service-text2">{HELP_DESK}</p>
-                            </span>
-                        </div>
+                            </p>
+                            <p className="service-text2">{HELP_DESK}</p>
+                        </span>
                     </div>
                 </div>
             </div>
